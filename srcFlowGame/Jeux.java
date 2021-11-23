ef1c4d2f93331490de656d6bb953f7b23f7939b5
MEREL ANNE p1905532 <anne.merel@etu.univ-lyon1.fr>
2021-11-23 14:10:04 +0100

    changemenets dans chemin

--- srcFlowGame/Jeux.java
+++ srcFlowGame/Jeux.java
@@ -3,7 +3,7 @@ import java.util.Observable;
 public class Jeux extends Observable {
     public int size;
     public CaseModele tab_jeu[][];
-    public int [] cheminCourante[]; // pour stocker le chemin courante
+   // public int [] cheminCourante[]; // pour stocker le chemin courante // misschien
 
 
     public Jeux (int size){
@@ -28,20 +28,40 @@ public class Jeux extends Observable {
     public void construireChemin(int x, int y){
          CaseType cm = this.tab_jeu[x][y].type;
 
-
-
         // verifier si une case est une case vide? --> type empty
-        if(cm == CaseType.empty){
-                ajouteCaseModelChemin();
-        }
         // verifier si on croise pas? --> type h...
-
         // verifier si une case est le fin ? --> exacte le meme type que le debut --> type S_
+         switch(cm){
+             // etats debut/finaux
+             case S1:
+             case S2:
+             case S3:
+             case S4:
+             case S5:
+                    initCaseModelChemin();
+                 break;
+
+             case empty:
+                 ajouteCaseModelChemin();
+                 System.out.println("case ajoute aux chemin");
+                 break;
+             // le reste des options h0h1, v0v1, cross, h0v0, h0v1, h1v0, h1v1
+             default:
+                 System.out.println("case déjà rempli --> croisement");
+                 break;
+
+
+
+         }
+
+
+
     }
 
-    public void ajouteCaseModelChemin(){
+    // reste a faire
+    public void ajouteCaseModelChemin(){}
 
-    }
+    public void initCaseModelChemin(){}
 
 
 }
