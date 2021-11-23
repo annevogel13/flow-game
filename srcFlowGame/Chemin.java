aadf2b45676711d4215be5f25740952aa80d8306
p1905073 <jade.marquette@etu.univ-lyon1.fr>
2021-11-23 12:27:08 +0100

    début écriture fonctions dans chemins

--- srcFlowGame/Chemin.java
+++ srcFlowGame/Chemin.java
@@ -1,2 +1,17 @@
 public class Chemin {
+    public char tab_chemins[];
+    public int niveau;
+    public int nbr_formes;
+
+    public Chemin(){
+
+    }
+
+    public void init_puzzle(){
+
+    }
+
+    public void recup_info_puzzle(){
+        
+    }
 }
