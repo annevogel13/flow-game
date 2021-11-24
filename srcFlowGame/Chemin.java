87a081792f1a8feb31a65fb7983fa71649cd57d0
p1905073 <jade.marquette@etu.univ-lyon1.fr>
2021-11-24 15:45:45 +0100

    rajout new dns chemin constructeur

--- srcFlowGame/Chemin.java
+++ srcFlowGame/Chemin.java
@@ -8,8 +8,33 @@ public class Chemin {
 
     public Chemin() {
         chemin_courant = new CaseModele[TAILLE_MAX];
+        for(int i = 0; i < TAILLE_MAX; i++){
+
+            chemin_courant[i] = new CaseModele();
+        }
     }
 
+    public boolean verif_case_voisine(){
+        CaseModele[] tab_case_voisine = new CaseModele[4];
+        tab_case_voisine[0].
+        for(int i = 0; i < taille_chemin_courant; i++){
 
+        }
+    }
+
+    public void calcule_coord_p_ou_m_un(int _x, int _y, CaseModele c){
+        CaseModele c2 = new CaseModele();
+        c.x = c.x + _x;
+        c.y = c.y + _y;
+    }
+
+    public boolean dedans_grille(CaseModele c){
+        if((c.x < 0)||(c.y < 0)||(c.x < taille_chemin_courant)||(c.y < taille_chemin_courant)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 
 }
