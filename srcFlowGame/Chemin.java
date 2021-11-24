4f71264a332ad28ec502cb5a9421173cb6a6a896
p1905073 <jade.marquette@etu.univ-lyon1.fr>
2021-11-24 15:07:14 +0100

    changement dans chemin

--- srcFlowGame/Chemin.java
+++ srcFlowGame/Chemin.java
@@ -2,10 +2,12 @@
 //ATTENTION quand on fait (1,2) ça fait en réalité (2,1) REPERE CHANGE
 
 public class Chemin {
+    CaseModele[] chemin_courant;
+    int taille_chemin_courant;
+    int TAILLE_MAX = 10;
 
     public Chemin() {
-
-
+        chemin_courant = new CaseModele[TAILLE_MAX];
     }
 
 
