057754b4a5eed66af9b9ec39c043832cee09652b
p1905073 <jade.marquette@etu.univ-lyon1.fr>
2021-11-28 20:49:20 +0100

    ajout des images pour l'affichage

--- srcFlowGame/VueCase.java
+++ srcFlowGame/VueCase.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
 // TODO : redéfinir la fonction hashValue() et equals(Object) si vous souhaitez utiliser la hashmap de VueControleurGrille avec VueCase en clef
@@ -11,7 +15,9 @@ public class VueCase extends JPanel {
     public CaseModele c;
 
     public VueCase(CaseModele cm) {
+
         c = cm;
+        chargement_images();
     }
 
     private void drawNoon(Graphics g) {
@@ -30,32 +36,44 @@ public class VueCase extends JPanel {
         g.drawLine(getWidth()/2, getHeight()/2, getWidth(), getHeight()/2);
     }
 
+    public Image img0 = null;    public Image img1 = null;    public Image img2 = null;    public Image img3 = null;
+    public Image img4 = null;    public Image img5 = null;    public Image img6 = null;    public Image img7 = null;    public Image img8 = null;
+
+
 
 
     @Override
     public void paintComponent(Graphics g) {
+
         g.clearRect(0, 0, getWidth(), getHeight());
 
         g.drawRoundRect(getWidth()/4, getHeight()/4, getWidth()/2, getHeight()/2, 5, 5);
 
         Rectangle2D deltaText =  g.getFont().getStringBounds("0", g.getFontMetrics().getFontRenderContext()); // "0" utilisé pour gabarit
 
+
+
                 //TODO rajoute S6 - S9
         switch(c.type) {
             case S1 :
-                g.drawString("1", getWidth()/2 - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
+                g.drawImage(img0,0, 0, getWidth(), getHeight(), this);
+                //g.drawString("1", getWidth()/2 - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
                 break;
             case S2 :
-                g.drawString("2", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
+                g.drawImage(img1,0, 0, getWidth(), getHeight(), this);
+                //g.drawString("2", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
                 break;
             case S3 :
-                g.drawString("3", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
+                g.drawImage(img2,0, 0, getWidth(), getHeight(), this);
+                //g.drawString("3", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
                 break;
             case S4 :
-                g.drawString("4", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
+                g.drawImage(img3,0, 0, getWidth(), getHeight(), this);
+                //g.drawString("4", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
                 break;
             case S5 :
-                g.drawString("5", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
+                g.drawImage(img4,0, 0, getWidth(), getHeight(), this);
+                //g.drawString("5", getWidth()/2  - (int) deltaText.getCenterX(), getHeight()/2 - (int) deltaText.getCenterY());
                 break;
             case h0v0 :
                 drawNine(g);
@@ -89,10 +107,38 @@ public class VueCase extends JPanel {
                 break;
 
         }
+
+
+
     }
     public String toString() {
         return c.x + ", " + c.y;
 
     }
+
+    public void chargement_images(){
+        System.out.println("dedans");
+
+        try {
+            img0 = ImageIO.read(new File("../data/images/vert.png"));
+            img1 = ImageIO.read(new File("../data/images/bleu.png"));
+            img2 = ImageIO.read(new File("../data/images/jaune.png"));
+            img3 = ImageIO.read(new File("../data/images/violet.jpg"));
+            img4 = ImageIO.read(new File("../data/images/creme.png"));
+            img5 = ImageIO.read(new File("../data/images/noir.png"));
+            img6 = ImageIO.read(new File("../data/images/orange.jpg"));
+            img7 = ImageIO.read(new File("../data/images/rose.png"));
+            img8 = ImageIO.read(new File("../data/images/rouge.jpg"));
+
+        } catch (IOException e) {
+            System.out.println("\nL'image ne pouvait pas charger. \n");
+        }
+
+
+
+
+
+    }
+
 
 }
