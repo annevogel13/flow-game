5e6446f6fea2c8e4ab3abba37e5f3c09c500cfa6
p1905073 <jade.marquette@etu.univ-lyon1.fr>
2021-11-30 10:49:03 +0100

    desing de la grille + essai debug de case aléatoire (pattern ? je crois pas) qui passe à empty pour le type_chemin No idea...

--- srcFlowGame/CaseModele.java
+++ srcFlowGame/CaseModele.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class CaseModele {
     public CaseType type;
+    public CaseType type_chemin;
     private static Random rnd = new Random();
     public int x, y, direction_case_suivante;
 
@@ -9,8 +10,11 @@ public class CaseModele {
         x = _i;
         y = _j;
         type = CaseType.empty; // @p1905073 j'ai ajoute ça pour faire fonctionner les chemins
+        type_chemin = CaseType.empty;
+
         // rndType();
        // System.out.println("new rnd type casemodele");
+
     }
 
     public void rndType() {
