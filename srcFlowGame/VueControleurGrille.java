01960a2e2484fbd05b107dc05c8d19e3d73f27f9
p1905073 <jade.marquette@etu.univ-lyon1.fr>
2021-12-07 21:24:32 +0100

    menu ok normalement

--- srcFlowGame/VueControleurGrille.java
+++ srcFlowGame/VueControleurGrille.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
@@ -18,6 +20,14 @@ public class VueControleurGrille extends JFrame implements Observer{
     // Afin d'accéder au composant sur lequel le bouton de souris est relaché, on le conserve avec la variable currentComponent à
     // chaque entrée dans un composant - voir (**)
     private JComponent currentComponent;
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu menu = new JMenu("Choix grille");
+
+    private JMenuItem g51;    private JMenuItem g52;    private JMenuItem g53;
+    private JMenuItem g61;    private JMenuItem g62;    private JMenuItem g63;
+    private JMenuItem g71;    private JMenuItem g72;    private JMenuItem g81;
+    private JMenuItem g91;
+
 
     public VueControleurGrille(Jeux jeu) {
         int size = jeu.size;
@@ -28,6 +38,136 @@ public class VueControleurGrille extends JFrame implements Observer{
 
         JPanel contentPane = new JPanel(new GridLayout(size, size));
 
+        menuBar.add(menu);
+
+
+        g51 = new JMenuItem(new AbstractAction(" Taille : 5  Niveau 1") {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Jeux _jeu = new Jeux(5,1);
+                    VueControleurGrille vue = new VueControleurGrille(_jeu);
+                    dispose();
+                    _jeu.addObserver(vue);
+                    vue.setVisible(true);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        });
+
+        g52 = new JMenuItem(new AbstractAction(" Taille : 5  Niveau 2") {
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(5,2);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        g53 = new JMenuItem(new AbstractAction(" Taille : 5  Niveau 3") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(5,3);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        g61 = new JMenuItem(new AbstractAction(" Taille : 6  Niveau 1") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(6,1);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        g62 = new JMenuItem(new AbstractAction(" Taille : 6  Niveau 2") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(6,2);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        g63 = new JMenuItem(new AbstractAction(" Taille : 6  Niveau 3") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(6,3);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        g71 = new JMenuItem(new AbstractAction(" Taille : 7  Niveau 1") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(7,1);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        g72 = new JMenuItem(new AbstractAction(" Taille : 7  Niveau 2") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(7,2);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        g81 = new JMenuItem(new AbstractAction(" Taille : 8  Niveau 1") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(8,1);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        g91 = new JMenuItem(new AbstractAction(" Taille : 9  Niveau 1") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    nouvelle_partie(9,1);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+
+
+        menu.add(g51);  menu.add(g52); menu.add(g53); menu.add(g61); menu.add(g62);
+        menu.add(g63);  menu.add(g71); menu.add(g72); menu.add(g81); menu.add(g91);
+
+        setJMenuBar(menuBar);
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -46,6 +186,7 @@ public class VueControleurGrille extends JFrame implements Observer{
                         //System.out.println("mousePressed : " + e.getSource());
                         try {
                             jeu.sourisCliquer(ci,cj);
+
                         } catch (CloneNotSupportedException ex) {
                             ex.printStackTrace();
                         }
@@ -117,5 +258,11 @@ public class VueControleurGrille extends JFrame implements Observer{
         vue.setVisible(true);
     }
 
-
+    public void nouvelle_partie(int taille, int niveau) throws IOException {
+        Jeux _jeu = new Jeux(taille,niveau);
+        VueControleurGrille vue = new VueControleurGrille(_jeu);
+        dispose();
+        _jeu.addObserver(vue);
+        vue.setVisible(true);
+    }
 }
